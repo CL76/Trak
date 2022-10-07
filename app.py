e1405c7b747edca96864764ca83f9d34a20b2b5a
CL76 <83503901+CL76@users.noreply.github.com>
2022-10-07 11:03:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ import streamlit as st
 df = pd.read_csv("data.txt",';')
 
 st.dataframe(df)
+st.write(df['type'].value_counts())
+
+from sklearn.linear_model import LogisticRegression
+#>>> X, y = load_iris(return_X_y=True)
+#>>> clf = LogisticRegression(random_state=0).fit(X, y)
+v>>> clf.predict(X[:2, :])
+#array([0, 0])
+#>>> clf.predict_proba(X[:2, :])
+#array([[9.8...e-01, 1.8...e-02, 1.4...e-08],
+#       [9.7...e-01, 2.8...e-02, ...e-08]])
+#>>> clf.score(X, y)
+#0.97...

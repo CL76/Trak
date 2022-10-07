d36b6c1e83f300fec292b50fd18414f96d85b757
CL76 <83503901+CL76@users.noreply.github.com>
2022-10-07 11:16:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.write(df.groupby('type').count())
 df_st1 = df[df['type']=='st1']
 
 st.write(df_st1)
-X=df_st1.loc['Nombre séance','dose/seance','Longueur Active La (cm)']
+X=df_st1.loc[:,['Nombre séance','dose/seance','Longueur Active La (cm)']]
 st.write(X)
 
  # clf = LogisticRegression(random_state=0).fit(X, y)

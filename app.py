b4f74277fddfb6d3c68bd72de8361043d247ef62
CL76 <83503901+CL76@users.noreply.github.com>
2022-10-07 10:39:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import time as ts
 from datetime import time
 from random import *
 
-from st_aggrid import AgGrid
 
-df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
+
+
 df_track = pd.read_csv("data.txt",';')
 st.df_track
 

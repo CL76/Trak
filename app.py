b50f41f718434703a0b4fe1917c97030d45f8476
CL76 <83503901+CL76@users.noreply.github.com>
2022-10-10 12:17:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,8 @@ st.write(df.groupby('type').size())
 st.write(df.groupby('type').count())
 df_st1 = df[df['type']=='st1']
 
-st.write(df_st1)
-X=df_st1.loc[:,['Nombre séance','dose/seance','Longueur Active La (cm)']]
-st.write(X)
-y=df_st1.loc[:,['KERMA (cgy cm²)']]
-st.write(y)
 
-clf = LogisticRegression(random_state=0).fit(X, y)
-predictions  = clf.predict(X[:2, :])
-st.write(predictions)
+
 
 st.title("Hello")
 fig = sns.countplot(x='type',data=df,palette='hls')
